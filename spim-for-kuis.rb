98f53f57c7ac7cb6bb1052932e56611cc8e214af
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-07-02 15:33:38 +0900

    Update spim-for-kuis for 9.1.17.1

--- spim-for-kuis.rb
+++ spim-for-kuis.rb
@@ -1,6 +1,9 @@
 class SpimForKuis < Formula
   desc "SPIM for Hardware and Software Laboratory Project 3 (Software)"
   homepage "https://github.com/ymyzk/spim-for-kuis"
+  url "https://github.com/ymyzk/spim-for-kuis/archive/v9.1.17.1.tar.gz"
+  sha256 "4641ab3dd14b076ec2a82264fdda6f1446da8c6c0d6a24c7cf897d1c88cc96f3"
+  version "9.1.17.1"
   head "https://github.com/ymyzk/spim-for-kuis.git"
 
   conflicts_with "spim"
