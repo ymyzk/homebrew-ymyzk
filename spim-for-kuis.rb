15aa812faeea4f4e6ff3ab2bbe98821c445529a0
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-10-04 15:59:04 +0900

    Fix spim-for-kuis.rb

--- spim-for-kuis.rb
+++ spim-for-kuis.rb
@@ -5,12 +5,12 @@ class SpimForKuis < Formula
   sha256 "4641ab3dd14b076ec2a82264fdda6f1446da8c6c0d6a24c7cf897d1c88cc96f3"
   head "https://github.com/ymyzk/spim-for-kuis.git"
 
-  conflicts_with "spim"
-
   bottle do
     root_url "https://github.com/ymyzk/spim-for-kuis/releases/download/v9.1.17.1"
     sha256 "e485c4d369663c2884a8b92b0940fc28f0de3d7ddd7b8ccb0343bf857332618a" => :el_capitan
   end
+
+  conflicts_with "spim"
 
   def install
     cd "spim"
